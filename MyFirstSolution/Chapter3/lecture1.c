0b0c42565d5e7a4ede4825b6f107bfea87254bbd
WonHS <wonhs717@gmail.com>
2019-08-04 18:27:25 +0900

    Create: CLFAL 3.03 Training / Create solution and write scanf example

---
+++ MyFirstSolution/Chapter3/lecture1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+
+	// scanf : Scan Formatted
+	scanf("%d", &i); // & : ampersand
+
+	printf("Value is %d\n", i);
+
+	return 0;
+}
