3104352e6b8554bf606f8715e29a4ff8f6fc7ee8
WonHS <wonhs717@gmail.com>
2019-08-04 15:47:06 +0900

    Chage: CLFAL 2.09 Training / Create printf function

--- MyFirstSolution/My3/my_third_program.c
+++ MyFirstSolution/My3/my_third_program.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+void say_hello(void); // prototyping, function declaration
 
 int main()
 {
+	bool test;
 	printf("My 3rd program");
 	return 0;
+}
+
+void say_hello(void) // function definition
+{
+	printf("Hello printf\n");
+
+	return;
 }
